5b7a75b0e93a5442ff6f8a4a0719b0be5ed8e1b9
Thai Pangsakulyanont @ Taskworld <thai.p@taskworld.com>
2017-02-02 15:11:30 +0700

    :art: standard

--- commit-status.js
+++ commit-status.js
@@ -1,70 +1,63 @@
 #!/usr/bin/env node
 
-var token = process.env.GH_STATUS_TOKEN || process.env.GH_TOKEN;
-var sha   = process.env.CIRCLE_SHA1 || process.env.TRAVIS_COMMIT;
-var owner = process.env.CIRCLE_PROJECT_USERNAME || extractSlug(process.env.TRAVIS_REPO_SLUG, 0);
-var repo  = process.env.CIRCLE_PROJECT_REPONAME || extractSlug(process.env.TRAVIS_REPO_SLUG, 1);
+var token = process.env.GH_STATUS_TOKEN || process.env.GH_TOKEN
+var sha = process.env.CIRCLE_SHA1 || process.env.TRAVIS_COMMIT
+var owner = process.env.CIRCLE_PROJECT_USERNAME || extractSlug(process.env.TRAVIS_REPO_SLUG, 0)
+var repo = process.env.CIRCLE_PROJECT_REPONAME || extractSlug(process.env.TRAVIS_REPO_SLUG, 1)
 
-
-var usage = ' (Usage: commit-status <state> <context> <description> [<target_url>])';
+var usage = ' (Usage: commit-status <state> <context> <description> [<target_url>])'
 
 validate(
   [token, 'GitHub token not configured (use GH_STATUS_TOKEN env var)'],
-  [sha,   'Unable to detect current build’s SHA'],
+  [sha, 'Unable to detect current build’s SHA'],
   [owner, 'Unable to detect repository owner'],
-  [repo,  'Unable to detect repository name'],
+  [repo, 'Unable to detect repository name'],
   [process.argv.length >= 5, 'Not enough arguments.' + usage],
   [process.argv.length <= 6, 'Too many arguments.' + usage]
-);
+)
 
-
-var state       = process.argv[2];
-var context     = process.argv[3];
-var description = process.argv[4];
-var targetUrl   = process.argv[5];
-var validStates = ['pending', 'success', 'error', 'failure'];
+var state = process.argv[2]
+var context = process.argv[3]
+var description = process.argv[4]
+var targetUrl = process.argv[5]
+var validStates = ['pending', 'success', 'error', 'failure']
 
 validate(
   [validStates.indexOf(state) >= 0, 'Invalid state (allowed: ' + state + ')']
-);
+)
 
-
-var Octokat = require('octokat');
-var octo    = new Octokat({ token: token });
+var Octokat = require('octokat')
+var octo = new Octokat({ token: token })
 
 octo.repos(owner, repo).statuses(sha).create({
-  state:        state,
-  context:      context,
-  description:  description,
-  target_url:   targetUrl || undefined,
+  state: state,
+  context: context,
+  description: description,
+  target_url: targetUrl || undefined
 }).catch(function (e) {
   setImmediate(function () {
-    throw e;
-  });
-});
-
+    throw e
+  })
+})
 
 function extractSlug (str, i) {
-
-  return str && str.split('/')[i];
+  return str && str.split('/')[i]
 }
 
-
 function validate () {
-
-  var problems = [ ];
+  var problems = [ ]
   for (var i = 0; i < arguments.length; i++) {
     if (!arguments[i][0]) {
-      problems.push(arguments[i][1]);
+      problems.push(arguments[i][1])
     }
   }
 
   if (problems.length) {
-    console.error('Unable to run:');
+    console.error('Unable to run:')
     problems.forEach(function (problem) {
-      console.error(' -', problem);
-    });
-    console.error('');
-    process.exit(1);
+      console.error(' -', problem)
+    })
+    console.error('')
+    process.exit(1)
   }
 }
